0cd88e44bbe846d81bf247cfae8ac24d67ca8738
MyUsernamee <delanoleslieprof@gmail.com>
2025-03-07 03:07:32 -0800

    Basic window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <raylib.h>
+#include <rlgl.h>
 
 int main() {
 
 	InitWindow(1280, 720, "Fractal Flyer");
+
+	while (!WindowShouldClose()) {
+
+		BeginDrawing();
+
+		EndDrawing();
+
+	}
+
+	CloseWindow();
 
 }
